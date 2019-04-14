7c1fa2863bead482f351b3f3e124427d8f4481af
Dmitry Tarasov <mrsndmn46@gmail.com>
2019-04-14 18:54:47 +0200

    queue

--- tasks.py
+++ tasks.py
@@ -1,13 +1,13 @@
 from celery import Celery
-from celery.contrib.batches import Batches
+from celery_batches import Batches
 
 # ~/pyvenvs/bin/celery -A tasks worker --loglevel=info
 
 app = Celery('tasks', broker='amqp://172.17.0.3:5672/')
 
-@app.task(base=Batches, flush_every=100, flush_interval=10)
+@app.task(base=Batches, flush_every=7, flush_interval=10)
 def save2arango(ev):
-    print("saving 2 arango: {}".format(ev))
+    print("saving 2 arango {} events: {}".format(len(ev), ev))
     return
 
 @app.task(base=Batches, flush_every=100, flush_interval=10)
