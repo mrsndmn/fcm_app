15cfba6ce4fb6049a48ae6abce0a4349ec9f706a
Dmitry Tarasov <d.tarasov@corp.mail.ru>
2019-04-14 19:10:22 +0300

    master batches support

--- tasks.py
+++ tasks.py
@@ -1,9 +1,21 @@
 from celery import Celery
+from celery.contrib.batches import Batches
+
+# ~/pyvenvs/bin/celery -A tasks worker --loglevel=info
 
 app = Celery('tasks', broker='amqp://172.17.0.3:5672/')
 
-@app.task
-def add(x, y):
-    print("{} + {} = {}".format(x, y, x + y))
-    return x + y
+@app.task(base=Batches, flush_every=100, flush_interval=10)
+def save2arango(ev):
+    print("saving 2 arango: {}".format(ev))
+    return
 
+@app.task(base=Batches, flush_every=100, flush_interval=10)
+def save2bigquery(ev):
+    print("saving 2 bigquery: {}".format(ev))
+    return
+
+@app.task(base=Batches, flush_every=100, flush_interval=10)
+def save2postgres(ev):
+    print("saving 2 pg: {}".format(ev))
+    return
